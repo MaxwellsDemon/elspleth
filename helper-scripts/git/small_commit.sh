e25783a6c79d3ee7350436410e112bede75573b6
Curtis Fleming <cfleming@kenzan.com>
2018-06-18 09:27:02 -0600

    This commit is tested with small_commit.sh itself

---
+++ helper-scripts/git/small_commit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+function usage() {
+	echo "usage: small_commit <new, full, branch name>"
+	exit 1
+}
+
+if [ $# -ne 1 ]; then
+	usage
+fi
+
+if git branch | grep develop > /dev/null
+then
+	git checkout develop
+fi
+git pull
+if git branch | grep "$1" > /dev/null
+then
+	git checkout "$1"
+else
+	# git checkout returns 0 if already on branch
+	# git checkout returns 128 if branch already exists
+	git checkout -b "$1"
+fi
+git add .
+git commit
+# TODO add the extra stuff for a proper branch track
+git push
+echo
+echo "Small commit done"
+echo

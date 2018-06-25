60d50d83e21ffa644de50c7023eeeba34a4048f5
Curtis Fleming <cfleming@kenzan.com>
2018-06-25 10:54:35 -0600

    More typical morning script

--- helper-scripts/holistic/morning.sh
+++ helper-scripts/holistic/morning.sh
@@ -2,12 +2,23 @@
 
 . ~/.bashrc_local_variables
 
-agit() {
+asap_git() {
+	"${code}"/elspleth/helper-scripts/git/qgit.sh "$@"
+}
+
+realtime_git() {
 	"${code}"/elspleth/helper-scripts/git/generic_all_git.sh "$@"
 }
 
 # The idea behind the Morning script is to sip a hot beverage and watch
-# repositories update one-by-one and grok changes.  That's why sequential 
-# agit is used instead of fast async qgit.
-agit fetch --prune
 
+cd "${code}"
+asap_git checkout develop
+echo 
+echo 
+echo 
+echo 
+echo "                             Fetching and pruning"
+echo
+#realtime_git fetch --prune
+realtime_git pull --prune

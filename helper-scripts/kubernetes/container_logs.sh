84b859e202a6da5e7c77ead2edf87c3aeb212eda
Curtis Fleming <cfleming@kenzan.com>
2018-07-30 13:11:26 -0600

    Handles case when pod base name differs from container name

--- helper-scripts/kubernetes/container_logs.sh
+++ helper-scripts/kubernetes/container_logs.sh
@@ -2,15 +2,16 @@
 
 # Assumes a default namespace for kubectl is set in ~/.kube/config
 
-if [ $# -ne 1 ];then
-    echo "usage: $(basename "$0") <container name>"
+if [ $# -lt 1 -o $# -gt 2 ];then
+    echo "usage: $(basename "$0") <pod name substring> [<container name>]"
     exit 1
 fi
 
 container_name="$1"
+if [ "$2" ]; then
+	container_name="$2"
+fi
 pod_name=$(kubectl get pods | grep "$1" | cut -f1 -d' ')
-
 kubectl logs "${pod_name}" -c "${container_name}" > "${container_name}".log
 kubectl logs -f "${pod_name}" -c "${container_name}"
-
 

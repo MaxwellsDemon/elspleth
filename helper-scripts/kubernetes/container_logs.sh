3bb3b80766063f0e4b5df6341bef4152bfcb0add
Curtis Fleming <cfleming@kenzan.com>
2018-08-07 14:26:03 -0600

    Container log script no longer polutes.

--- helper-scripts/kubernetes/container_logs.sh
+++ helper-scripts/kubernetes/container_logs.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+trap ctrl_c INT
+
 scripts_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 source "${scripts_dir}/pod_and_container_command.sh"
-kubectl logs -n "${namespace}" "${pod_name}" -c "${container_name}" > "${container_name}".log
+
+function ctrl_c() {
+	echo
+	echo
+	echo "Use this command to get the entire logs"
+	echo "kubectl logs -n \"${namespace}\" \"${pod_name}\" -c \"${container_name}\" > \"${container_name}\".log"
+	echo
+}
+
 kubectl logs -n "${namespace}" -f "${pod_name}" -c "${container_name}"
 

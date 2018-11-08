63112b870a15760590ef33efb2992c889ab91242
Curtis Fleming <C-Curtis.Fleming@charter.com>
2018-11-08 11:28:07 -0700

    Choices to get kube logs

--- helper-scripts/kubernetes/container_logs.sh
+++ helper-scripts/kubernetes/container_logs.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
-trap ctrl_c INT
+set -e
 
 scripts_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 source "${scripts_dir}/pod_and_container_command.sh"
 
-function ctrl_c() {
-	echo
-	echo
-	echo "Use this command to get the entire logs"
-	echo "kubectl logs -n \"${namespace}\" \"${pod_name}\" -c \"${container_name}\" > \"${container_name}\".log"
-	echo
-}
-
-kubectl logs -n "${namespace}" -f "${pod_name}" -c "${container_name}"
+echo
+echo "PICK YOUR POISON"
+echo
+echo "MAY NEVER FINISH"
+echo "kubectl logs -n \"${namespace}\" \"${pod_name}\" -c \"${container_name}\" > \"${container_name}\".log && vi \"${container_name}\".log"
+echo
+echo "LAST 1000 LINES"
+echo "kubectl logs -n \"${namespace}\" --tail=1000 \"${pod_name}\" -c \"${container_name}\" > \"${container_name}\".log && vi \"${container_name}\".log"
 

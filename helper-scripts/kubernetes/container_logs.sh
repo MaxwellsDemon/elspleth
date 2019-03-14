ba8682c3152510393e269bbf48bce00a7be2e1e6
Curtis Fleming <C-Curtis.Fleming@charter.com>
2019-03-14 15:45:01 -0600

    pod specific logs

--- helper-scripts/kubernetes/container_logs.sh
+++ helper-scripts/kubernetes/container_logs.sh
@@ -4,12 +4,13 @@ set -e
 scripts_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 source "${scripts_dir}/pod_and_container_command.sh"
 
+logfile="${pod_name}.${container_name}.log"
 echo
 echo "PICK YOUR POISON"
 echo
 echo "MAY NEVER FINISH"
-echo "kubectl logs -n \"${namespace}\" \"${pod_name}\" -c \"${container_name}\" > \"${container_name}\".log && vi \"${container_name}\".log"
+echo "kubectl logs -n \"${namespace}\" \"${pod_name}\" -c \"${container_name}\" > \"${logfile}\" && vi \"${logfile}\""
 echo
 echo "LAST 1000 LINES"
-echo "kubectl logs -n \"${namespace}\" --tail=1000 \"${pod_name}\" -c \"${container_name}\" > \"${container_name}\".log && vi \"${container_name}\".log"
+echo "kubectl logs -n \"${namespace}\" --tail=1000 \"${pod_name}\" -c \"${container_name}\" > \"${logfile}\" && vi \"${logfile}\""
 

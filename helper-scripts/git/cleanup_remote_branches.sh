1165dff5c804d8305de09d423ef11b5a5f09af67
Curtis Fleming <C-Curtis.Fleming@charter.com>
2019-04-22 12:19:53 -0600

    Much nicer and test git branch cleanup

--- helper-scripts/git/cleanup_remote_branches.sh
+++ helper-scripts/git/cleanup_remote_branches.sh
@@ -1,37 +1,91 @@
 #!/bin/bash
+
+# Assumes git repos with only one remote, namely 'origin'
+
 set -e
 
+usage() {
+  echo "usage: $(basename "$0") <mode>"
+  echo "Modes: list, delete"
+  exit 1
+}
+
 reset_totals() {
-  total_branches=$(git branch -r | wc -l)
-  merged_branch_is_evergreen=0
-  merged_branch_two_weeks_young=0
-  merged_branch_deleteable=0
+  total_branches_count=$(git branch -r | wc -l)
+  merged_branch_is_evergreen_count=0
+  merged_branch_two_weeks_young_count=0
+  deleteable_merged_branch_count=0
+  deleteable_merged_branches=()
 }
 
 print_totals() {
-  echo
-  echo "Total branches: ${total_branches}"
-  echo "Not deleting merged branches that are two weeks young: ${merged_branch_two_weeks_young}"
-  echo "This leaves ${merged_branch_deleteable} branch(es) for removal"
+  section "Totals"
+  echo "Total remote branches: ${total_branches_count}"
+  echo "Branches that would be deleted but were merged less than two weeks ago: ${merged_branch_two_weeks_young_count}"
+  echo "Branches for removal: ${deleteable_merged_branch_count}"
+}
+
+validate() {
+  section "Validation"
+  echo "In directory $(pwd)"
+  if [ ! -d .git ]; then
+    echo "Not a git project. Please change directory to root git project."
+    exit -22
+  fi
+  if [ -n "$(git status --porcelain)" ]; then
+    echo "Please clean git workspace"
+    exit -33
+  fi
+}
+
+checkout_develop_or_master() {
+  section "Checkout of develop or master"
+  git fetch
+  if git checkout develop > /dev/null 2>&1 ; then
+    echo "The develop branch exists, use it as the base."
+  else
+    echo "The develop branch does not exist, using master as the base."
+    mute git checkout master
+  fi
+  git pull
+  core_branch="$(git rev-parse --abbrev-ref HEAD)"
+}
+
+gather_deleteable_branches() {
+  section "Gathering branches"
+  echo "Only processing branches already merged into ${core_branch}"
+  local merged_branches="$(git branch -r --merged "${core_branch}")"
+  while read raw_branch
+  do
+    local norm_branch=$(normalize_branch_name "${raw_branch}")
+    local branch=$(strip_origin "${norm_branch}")
+    if is_branch_name_malformed "${branch}"; then
+      continue
+    else
+      echo
+      echo "Branch ${raw_branch}"
+    fi
+    if is_branch_evergreen "${branch}"; then
+      ((merged_branch_is_evergreen_count++))
+      echo "Evergreen. Skipping."
+    elif is_branch_younger_than_two_weeks "${norm_branch}"; then
+      ((merged_branch_two_weeks_young_count++))
+      echo "Merged less than two weeks ago. Skipping."
+    else
+      echo "Deleteable"
+      ((deleteable_merged_branch_count++))
+      deleteable_merged_branches+=("${branch}")
+    fi
+  done <<< "${merged_branches}"
 }
 
 for_each_deleteable_branch() {
-  for rawBranch in $(git branch -r --merged develop); do
-    local norm_branch=$(normalize_branch_name "${rawBranch}")
-    local branch=$(strip_origin "${norm_branch}")
-    if is_branch_name_malformed "${branch}"; then
-      echo "Skipping malformed branch entry: ${rawBranch}"
-    elif is_branch_evergreen "${branch}"; then
-      ((merged_branch_is_evergreen++))
-      echo "Skipping evergreen branch: ${branch}"
-    elif is_branch_younger_than_two_weeks "${norm_branch}"; then
-      ((merged_branch_two_weeks_young++))
-      echo "Skipping younger than 2 week branch: ${branch}"
-    else
-      ((merged_branch_deleteable++))
-      "$@" "${branch}"
-    fi
+  section "List / Delete"
+  echo "========================================================================"
+  for branch in "${deleteable_merged_branches[@]}" ; do
+    "$@" "${branch}"
   done
+  echo "========================================================================"
 }
 
 normalize_branch_name() {
@@ -85,7 +139,18 @@ assert_equals() {
   fi
 }
 
+mute() {
+  "$@" > /dev/null 2>&1
+}
+
+section() {
+  echo
+  echo -e "\x1B[93m$1\x1B[0m"
+  echo
+}
+
 unit_test() {
+  section "Unit Tests"
   set +e
   assert_true is_branch_evergreen master
   assert_true is_branch_evergreen develop
@@ -97,19 +162,47 @@ unit_test() {
   assert_equals 'origin/my_feature' $(normalize_branch_name '   origin/my_feature')
   assert_equals 'my_feature' $(strip_origin 'origin/my_feature')
   # quasi-stable check that develop branch is actively updated
-  assert_true is_branch_younger_than_two_weeks develop
+  (assert_true is_branch_younger_than_two_weeks develop)
   echo "Unit tests pass"
   set -e
 }
 
-action() {
-  echo "DELETEABLE BRANCH: $1"
-  # git checkout "$1"
-  # git push origin :"$1"
+list_action() {
+  echo "Deleteable branch: $1"
 }
 
+delete_action() {
+  echo "Deleting branch: $1"
+  mute git checkout "$1"
+  mute git checkout "${core_branch}"
+  # Extra safe deletion check with '-d' instead of '-D' before remote delete
+  git branch -d "$1"
+  git push origin :"$1"
+}
+
+if [ $# -ne 1 ] ; then
+  usage
+fi
+
+case "$1" in
+  list)
+  mode=list_action
+  ;;
+  delete)
+  mode=delete_action
+  ;;
+  *)
+  "Unknown option: $1"
+  usage
+  exit 2
+  ;;
+esac
+
 unit_test
+validate
+checkout_develop_or_master
 reset_totals
-for_each_deleteable_branch action
+gather_deleteable_branches
+for_each_deleteable_branch "${mode}"
 print_totals
 
--- helper-scripts/learning/loop-over-lines-with-spaces.sh
+++ helper-scripts/learning/loop-over-lines-with-spaces.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# GOOD VERSION: variable modifications inside loop are visible outside loop
+
+myContent="$(find . -type f -name "*helloworld*" -print)"
+while read entry
+do
+  echo "Found: $entry"
+done <<< "${myContent}"
+
+# PROBLEMATIC VERSION: while-loop runs in subshell, so variable modifications are not visible outside loop
+
 find . -type f -name "*helloworld*" -print | while read entry
 do
   echo "Found: $entry"

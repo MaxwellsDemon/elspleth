57d906cb79594d255742dcb736fa4e018166a53d
Curtis Fleming <C-Curtis.Fleming@charter.com>
2020-12-14 09:00:50 -0700

    date examples

--- helper-scripts/learning/safe-date.sh
+++ helper-scripts/learning/safe-date.sh
@@ -3,3 +3,9 @@ set -x
 
 date "+%Y-%m-%d-%H-%M-%S"
 
+echo "universal, with milliseconds, and simpler punctuation, nanoseconds did not work on charter Gitlab runner"
+date --utc +"%Y-%m-%d_%H-%M-%S-%3N"
+
+echo "so this is safer but only second resolution
+date --utc +"%Y-%m-%d_%H-%M-%S"
+

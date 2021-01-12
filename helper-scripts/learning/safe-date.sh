f660d4d7832968b4a001f368a961f0d1fa6c9f27
Curtis Fleming <C-Curtis.Fleming@charter.com>
2021-01-12 13:37:35 -0700

    Cleaner date logging

--- helper-scripts/learning/safe-date.sh
+++ helper-scripts/learning/safe-date.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+
 set -x
-
 date "+%Y-%m-%d-%H-%M-%S"
+set +x
 
+echo
 echo "universal, with milliseconds, and simpler punctuation, nanoseconds did not work on charter Gitlab runner"
+set -x
 date --utc +"%Y-%m-%d_%H-%M-%S-%3N"
+set +x
 
-echo "so this is safer but only second resolution
+echo
+echo "so this is safer but only second resolution"
+set -x
 date --utc +"%Y-%m-%d_%H-%M-%S"
+set +x
 

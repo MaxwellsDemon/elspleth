48536b69681d8f6e5eeec799806551f8bda2803f
Curtis Fleming <C-Curtis.Fleming@charter.com>
2021-03-07 11:33:37 -0700

    Essential `local` declaration for loop variables!

--- helper-scripts/learning/array-of-lined-output.sh
+++ helper-scripts/learning/array-of-lined-output.sh
@@ -7,13 +7,16 @@ print_data_with_spaces() {
 }
 
 print_parameters() {
+  local param # ESSENTIAL to not break outer-loop variables
   for param in "$@"; do
     echo "Parameter: ${param}"
   done
 }
 
 mapfile -t my_array < <(print_data_with_spaces)
+param=globalvalue
 print_parameters "${my_array[@]}"
+echo "The global variable [param] has value [${param}]"
 
 echo; echo; echo; echo
 cat array-of-lined-output.sh

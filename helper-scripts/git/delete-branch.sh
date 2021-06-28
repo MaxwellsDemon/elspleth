20b98febdc70c406bf216c376212b93b57b7f6fa
Curtis Fleming <C-Curtis.Fleming@charter.com>
2021-06-28 15:54:23 -0600

    Faster branch delete. Handles masterless repos. Clear recovery hash. Fewer times local repo is left in a partial state.

--- helper-scripts/git/delete-branch.sh
+++ helper-scripts/git/delete-branch.sh
@@ -3,14 +3,29 @@
 set -e
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 source "${script_dir}/git-library.sh"
+validate_current_directory_is_clean_git_root
+core_branch="$(current_branch)"
+
+default_branches=(
+develop
+master
+main
+"$(git branch | sed 's/[\* ]//g' | grep -Po '^(?!'"${core_branch}"').+$' | head -n 1)"
+)
 
 confirm() {
   read -p 'Accept or ctrl-C >'
 }
 
-validate_current_directory_is_clean_git_root
+checkout_default_branch() {
+  local branch
+  for branch in "${default_branches[@]}"; do
+    if mute git checkout "${branch}"; then
+      return 0
+    fi
+  done
+}
 
-core_branch="$(current_branch)"
 case "${core_branch}" in
   master|develop)
   echo "Will not delete ${core_branch}"
@@ -18,16 +33,20 @@ case "${core_branch}" in
   ;;
 esac
 
+(
+  git fetch
+  git merge || true
+  echo "✨ Recovery hash $(git rev-parse "${core_branch}")"
+) 1>_temp_delete_log 2>&1 &
+
 echo "Delete local and remote branch ${core_branch}?"
 confirm
+wait
+cat _temp_delete_log
+rm _temp_delete_log
 
-git fetch
-git merge || true
 git push origin :"${core_branch}" || true
-checkout_develop_or_master
+checkout_default_branch
 git merge
-if ! git branch -d "${core_branch}" ; then
-  confirm
-  git branch -D "${core_branch}"
-fi
+git branch -D "${core_branch}"
 
